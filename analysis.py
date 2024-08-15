650b820d4602ba2a2abcaed00f56540c810ce9b1
IgorTatarnikov <igor.tatarnikov@gmail.com>
2024-08-15 15:04:22 +0100

    Initial commit

---
+++ analysis.py
@@ -0,0 +1,5 @@
+from my_funcs import square_add_10, print_twice
+
+print(square_add_10(2))
+
+print_twice("Hello")
---
+++ my_funcs.py
@@ -0,0 +1,9 @@
+def square_add_10(x):
+    y = x * x
+    y = y + 10
+    return y
+
+def print_twice(a_string):
+    print(a_string)
+    print(a_string)
+
