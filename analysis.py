6e2a760a9222e8b7d509f1d68022c9fc975ae07a
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2025-09-18 11:49:22 +0100

    Reset repo to use quarto template

--- analysis.py
+++
@@ -1,5 +0,0 @@
-from my_funcs import square_add_10, print_twice
-
-print(square_add_10(2))
-
-print_twice("Hello")
--- my_funcs.py
+++
@@ -1,9 +0,0 @@
-def square_add_10(x):
-    y = x * x
-    y = y + 10
-    return y
-
-def print_twice(a_string):
-    print(a_string)
-    print(a_string)
-
